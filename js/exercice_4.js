fbb6e64a887a9569cc858d519c6d3d1978b30fe5
Ilariosenpai <ilanabougit2@gmail.com>
2023-11-23 15:53:34 +0100

    exo4

---
+++ js/exercice_4.js
@@ -0,0 +1,5 @@
+let resultat1 = prompt("entrez votre Prénom");
+let resultat2 = prompt("entrez votre Nom");
+let resultat3 = prompt("entrez votre Ville");
+
+alert("bonjour, " + resultat1 + " " + resultat2 + ", quel temps il fait à " + resultat3 + " ?" );
