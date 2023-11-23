c8e45486e89f7ded223b0d17069044f9d038d7c5
Ilariosenpai <ilanabougit2@gmail.com>
2023-11-23 15:11:51 +0100

    exo1 et 2

---
+++ js/exercice_1_2.js
@@ -0,0 +1,12 @@
+let wesh = "Salut la team ça dit quoi ?"
+let lastName = "Abougit"
+let firstName = "Ilan"
+let city = "Saint-Etienne"
+
+
+
+
+
+
+alert( "Nom : " + lastName + "\n" + "Prénom : " + firstName + "\n" + "Ville : " + city 
+);
