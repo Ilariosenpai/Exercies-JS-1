18dd94eb9289d800d36b10ad02ef7417d39f02c8
Ilariosenpai <ilanabougit2@gmail.com>
2023-11-24 09:26:55 +0100

    exo7

---
+++ js/exercice_7.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+function exo7() {
+
+    let pointure = document.getElementById("shoeSize").value;
+    let année = document.getElementById("yearOfBirth").value;
+
+    let result = (pointure * 2 + 5) * 50 - année + 1766
+    
+
+    alert("le résultat est " + result)
+}
+
+
+
