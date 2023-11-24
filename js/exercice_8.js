7f5cae3c025c1c912fde34fa1173d00673470d94
Ilariosenpai <ilanabougit2@gmail.com>
2023-11-24 09:33:20 +0100

    exo8

---
+++ js/exercice_8.js
@@ -0,0 +1,18 @@
+function exo8() {
+
+let age = document.getElementById("age").value;
+
+if (age < 18) {
+
+
+    alert("Vous êtes mineur")
+    
+}
+
+else{
+
+    alert("Vous êtes majeur")
+}
+
+    
+}
