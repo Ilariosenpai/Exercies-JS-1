a1a0ad4a6d8cf6a3500e99be2e73e605146c1881
Ilariosenpai <ilanabougit2@gmail.com>
2023-11-24 13:58:51 +0100

    exo11

---
+++ js/exercice_11.js
@@ -0,0 +1,20 @@
+let chiffre = prompt("Veuillez renseignez un chiffre entre 1 et 10")
+
+
+if (chiffre < 5) {
+
+    alert("Le nombre saisie est inférieur à 5")
+    
+}
+
+else if (chiffre > 5 && chiffre < 10) {
+
+
+    alert("le chiffre est supérieur à 5")
+    
+}
+
+else if (chiffre > 10 ){
+
+    alert("J'ai dit entre 1 et 10 sale segpa")
+}
